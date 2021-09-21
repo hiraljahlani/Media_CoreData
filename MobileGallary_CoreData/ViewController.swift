c1c967ee41aadceb4168ffdf8a5928d85e2c8054
Hiral Jahlani <hiraljahlani@gmail.com>
2021-09-21 13:50:43 +0530

    Initial Commit

---
+++ MobileGallary_CoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobileGallary_CoreData
+//
+//  Created by Hiral Jahlani on 21/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
